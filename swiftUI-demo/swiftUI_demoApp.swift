fd05369aaff1ea9b3e5d5c4ebc3b49a99449206f
doanquang <quangdoanvan@tcom.vn>
2025-05-06 14:33:01 +0700

    Initial Commit

---
+++ swiftUI-demo/swiftUI_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_demoApp.swift
+//  swiftUI-demo
+//
+//  Created by Đoàn Văn Quang on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftUI-demoTests/swiftUI_demoTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_demoTests.swift
+//  swiftUI-demoTests
+//
+//  Created by Đoàn Văn Quang on 6/5/25.
+//
+
+import Testing
+@testable import swiftUI_demo
+
+struct swiftUI_demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
